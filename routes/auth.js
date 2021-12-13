b4fce19d3d36ea81b8b5a61f85bfc249e6a38f81
jalhagoit <pregdyt@gmail.com>
2021-12-13 18:23:59 +0900

    (판매자, 구매자) 로그인

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,8 @@ const hashedPassword = async (req) => {
     return await bcrypt.hash(req.body.password, salt); 
 };
 
-// TODO
-// 회원가입 시 아이디 중복 확인
+// TODO 회원가입 시
+// 아이디 중복 확인
 // 이메일 중복 확인
 // 입력한 비번과 컨펌 비번 일치 확인
 
@@ -52,5 +52,43 @@ router.post('/register/seller', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+
+
+// 구매자 로그인
+router.post('/login/buyer', async (req, res) => {
+    try {
+        const buyer = await Buyer.findOne({ username: req.body.username });
+        !buyer && res.status(401).json('일치하는 유저네임 없음');
+        
+        const validPassword = await bcrypt.compare(req.body.password, buyer.password);
+        !validPassword && res.status(401).json('비번 틀림');
+        
+        const { password, ...others } = buyer._doc;
+
+        res.status(200).json(others);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// 판매자 로그인
+router.post('/login/seller', async (req, res) => {
+    try {
+        const seller = await Seller.findOne({ username: req.body.username });
+        !seller && res.status(401).json('일치하는 유저네임 없음');
+        
+        const validPassword = await bcrypt.compare(req.body.password, seller.password);
+        !validPassword && res.status(401).json('비번 틀림');
+        
+        const { password, ...others } = seller._doc;
+
+        res.status(200).json(others);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+
 
 module.exports = router;
